076083f54a7cedd41b223a07e2bab6e049ddf428
Oligarch E <>
2021-09-18 14:48:50 -0700

    Add "mergeable" logic to common/codec - Basic types for mergeable codec - Implementations for boolean[And|Or], array, partial, type, union - Basic tests for implementations - Intersection remains a TODO, proxying overloaded args is annoying

---
+++ src/common/codec/mergeable.test.ts
@@ -0,0 +1,157 @@
+import * as t from 'io-ts';
+import * as mergeable from './mergeable';
+
+describe('basic', () => {
+    test('booleanAnd', () => {
+        expect(mergeable.booleanAnd.merge(true, true)).toBe(true);
+        expect(mergeable.booleanAnd.merge(true, false)).toBe(false);
+        expect(mergeable.booleanAnd.merge(false, true)).toBe(false);
+        expect(mergeable.booleanAnd.merge(false, false)).toBe(false);
+    });
+
+    test('booleanOr', () => {
+        expect(mergeable.booleanOr.merge(true, true)).toBe(true);
+        expect(mergeable.booleanOr.merge(true, false)).toBe(true);
+        expect(mergeable.booleanOr.merge(false, true)).toBe(true);
+        expect(mergeable.booleanOr.merge(false, false)).toBe(false);
+    });
+
+    test('array', () => {
+        const StringArray = mergeable.array(t.string);
+        
+        const empty = [];
+        const abc = [ 'a', 'b', 'c' ];
+        const def = [ 'd', 'e', 'f' ];
+
+        expect(StringArray.merge(empty, empty)).toEqual([]);
+        expect(StringArray.merge(abc, empty)).toEqual([ 'a', 'b', 'c' ]);
+        expect(StringArray.merge(empty, def)).toEqual([ 'd', 'e', 'f' ]);
+        expect(StringArray.merge(abc, def)).toEqual([ 'a', 'b', 'c', 'd', 'e', 'f' ]);
+    });
+
+    test('partial', () => {
+        const TestCodec = mergeable.partial({ a: t.boolean });
+
+        const empty = {};
+        const aTrue = { a: true };
+        const aFalse = { a: false };
+
+        expect(TestCodec.merge(empty, empty)).toEqual({});
+        expect(TestCodec.merge(aTrue, empty)).toEqual({ a: true });
+        expect(TestCodec.merge(empty, aTrue)).toEqual({ a: true });
+        expect(TestCodec.merge(aTrue, aFalse)).toEqual({ a: false });
+        expect(TestCodec.merge(aFalse, aTrue)).toEqual({ a: true });
+    });
+
+    test('type', () => {
+        const TestCodec = mergeable.type({ a: t.boolean });
+
+        const aTrue = { a: true };
+        const aFalse = { a: false };
+
+        expect(TestCodec.merge(aTrue, aFalse)).toEqual({ a: false });
+        expect(TestCodec.merge(aFalse, aTrue)).toEqual({ a: true });
+    });
+
+    test('union', () => {
+        const StringOrNumber = mergeable.union([ t.string, t.number ]);
+
+        const oneStr = 'one';
+        const oneNum = 1;
+        
+        const twoStr = 'two';
+        const twoNum = 2;
+
+        expect(StringOrNumber.merge(oneStr, oneNum)).toEqual(1);
+        expect(StringOrNumber.merge(oneNum, oneStr)).toEqual('one');
+        expect(StringOrNumber.merge(oneStr, twoStr)).toEqual('two');
+        expect(StringOrNumber.merge(oneNum, twoNum)).toEqual(2);
+    });
+
+    test.todo('intersection');
+});
+
+describe('basic nested', () => {
+    const pArray = t.array(t.string);           // Primitive array
+    const mArray = mergeable.array(t.string);   // Mergeable array
+
+    test('partial contains array', () => {
+        const TestCodec = mergeable.partial({ a: pArray, b: mArray });
+
+        const merged = TestCodec.merge(
+            { a: [ 'elem1' ], b: [ 'elem1' ] },
+            { a: [ 'elem2' ], b: [ 'elem2' ] },
+        );
+
+        expect(merged).toEqual({
+            a: [ 'elem2' ],
+            b: [ 'elem1', 'elem2' ],
+        });
+    });
+
+    test('type contains array', () => {
+        const TestCodec = mergeable.type({ a: pArray, b: mArray });
+
+        const merged = TestCodec.merge(
+            { a: [ 'elem1' ], b: [ 'elem1' ] },
+            { a: [ 'elem2' ], b: [ 'elem2' ] },
+        );
+
+        expect(merged).toEqual({
+            a: [ 'elem2' ],
+            b: [ 'elem1', 'elem2' ],
+        });
+    });
+
+    test('union included array', () => {
+        const TestCodec = mergeable.union([ t.string, mArray ]);
+
+        expect(TestCodec.merge('elem1', 'elem2')).toEqual('elem2');
+        expect(TestCodec.merge('elem1', [ 'elem2' ])).toEqual([ 'elem2' ]);
+        expect(TestCodec.merge([ 'elem1' ], 'elem2')).toEqual('elem2');
+        expect(TestCodec.merge([ 'elem1' ], [ 'elem2' ])).toEqual([ 'elem1', 'elem2' ]);
+    });
+
+    test.todo('intersection includes ...');
+});
+
+describe('complex nested', () => {
+    test('involves type, partial, union, array', () => {
+        const mArray = mergeable.array(t.string);
+    
+        const TestCodec = mergeable.type({
+            keyOne: mergeable.partial({ a: t.boolean, b: t.boolean, c: mArray }),
+            keyTwo: mergeable.type({ d: t.boolean, e: t.boolean, f: mArray }),
+            keyThree: mergeable.union([
+                mergeable.type({ yi: mArray, er: mArray }),
+                mergeable.type({ san: mArray, si: mArray }),
+            ]),
+            keyFour: mergeable.union([
+                mergeable.type({ liu: mArray, qi: mArray }),
+                mergeable.type({ ba: mArray, jiu: mArray }),
+            ]),
+            // TODO: intersection
+        });
+    
+        const a: t.TypeOf<typeof TestCodec> = {
+            keyOne: { a: true, b: true, c: [ 'elem1' ] },
+            keyTwo: { d: true, e: true, f: [ 'elem1' ] },
+            keyThree: { yi: [ 'elem1' ], er: [ 'elem1' ] },
+            keyFour: { liu: [ 'elem1' ], qi: [ 'elem1' ] },
+        };
+    
+        const b: t.TypeOf<typeof TestCodec> = {
+            keyOne: { a: false, b: false, c: [ 'elem2' ] },
+            keyTwo: { d: false, e: false, f: [ 'elem2' ] },
+            keyThree: { san: [ 'elem2' ], si: [ 'elem2' ] },
+            keyFour: { liu: [ 'elem2' ], qi: [ 'elem2' ] },
+        };
+    
+        expect(TestCodec.merge(a, b)).toEqual({
+            keyOne: { a: false, b: false, c: [ 'elem1', 'elem2' ] },
+            keyTwo: { d: false, e: false, f: [ 'elem1', 'elem2' ] },
+            keyThree: { san: [ 'elem2' ], si: [ 'elem2' ] },
+            keyFour: { liu: [ 'elem1', 'elem2' ], qi: [ 'elem1', 'elem2' ] },
+        });
+    });
+});
---
+++ src/common/codec/mergeable.ts
@@ -0,0 +1,85 @@
+import * as t from 'io-ts';
+
+export type Function<T> = (a: T, b: T) => T;
+
+export class Type<A, O, I> extends t.Type<A, O, I> {
+    readonly _tag: 'MergeableType' = 'MergeableType';
+
+    constructor (
+        name: string,
+        is: Type<A, O, I>['is'],
+        validate: Type<A, O, I>['validate'],
+        encode: Type<A, O, I>['encode'],
+        readonly merge: Function<A>,
+    ) { super(name, is, validate, encode) }
+
+    static from = <C extends t.Any> (
+        codec: C,
+        merge: Function<t.TypeOf<C>>,
+        name = `Mergeable<${codec.name}>`,
+    ) => new Type<t.TypeOf<C>, t.OutputOf<C>, t.InputOf<C>>(name, codec.is, codec.validate, codec.encode, merge);
+}
+
+export interface C<A, O, I> extends Type<A, O, I> {};
+
+export const isMergeableC = <A, O, I> (codec: t.Type<A, O, I>): codec is C<A, O, I> => (codec as any)._tag === 'MergeableType';
+
+type MergeOfProps<P extends t.AnyProps> = { [K in keyof P]: Function<t.TypeOf<P[K]>> }
+
+function propsMergeFuncs<P extends t.AnyProps> (props: P, defaultF: Function<t.TypeOf<P[string]>>): MergeOfProps<P> {
+    return Object.entries(props).reduce((acc, [key, val]) => {
+        acc[key] = isMergeableC(val) ? val.merge : defaultF;
+        return acc;
+    }, {} as any);
+}
+
+export const booleanAnd = Type.from(t.boolean, (a, b) => a && b);
+export const booleanOr = Type.from(t.boolean, (a, b) => a || b);
+
+export const array = <C extends t.Mixed>(item: C, name?: string) => Type.from(
+    t.array(item, name),
+    (a, b) => a.concat(b),
+);
+
+export const partial = <P extends t.Props>(props: P, name?: string) => {
+    const propKeys: (keyof P)[] = Object.keys(props);
+    const mergeFuncs = propsMergeFuncs(props, (a, b) => (b === undefined) ? a : b);
+
+    return Type.from(
+        t.partial(props, name),
+        (a, b) => propKeys.reduce((acc, cur) => {
+            acc[cur] = mergeFuncs[cur](a[cur], b[cur]);
+            return acc;
+        }, {} as t.TypeOfPartialProps<P>),
+    );
+};
+
+export const type = <P extends t.Props>(props: P, name?: string) => {
+    const propKeys: (keyof P)[] = Object.keys(props);
+    const mergeFuncs = propsMergeFuncs(props, (_, b) => b);
+
+    return Type.from(
+        t.type(props, name),
+        (a, b) => propKeys.reduce((acc, cur) => {
+            acc[cur] = mergeFuncs[cur](a[cur], b[cur]);
+            return acc;
+        }, {} as t.TypeOfProps<P>),
+    );
+};
+
+export const union = <CS extends [ t.Mixed, t.Mixed, ...Array<t.Mixed> ]>(codecs: CS, name?: string) => {
+    const codecFor = (x: t.TypeOf<CS[number]>) => codecs.find(c => c.is(x)) as CS[number];
+
+    return Type.from(
+        t.union(codecs, name),
+        (a, b) => {
+            const aCodec = codecFor(a);
+            return (aCodec.is(b) && isMergeableC(aCodec))
+                ? aCodec.merge(a, b)
+                : b;
+        },
+    );
+};
+
+// TODO: intersection
+// > Proxying the overloaded arguments is proving annoying
